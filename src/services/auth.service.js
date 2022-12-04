be3b9d63e0dc304a87165fad93ee103e58c9682f
talu <talu.dev@gmail.com>
2022-12-05 05:13:37 +0700

    fix: remove file

--- src/services/auth.service.js
+++
@@ -1,8 +0,0 @@
-import api from "./api";
-
-const AuthAPI = {
-	login: async (username, password) =>
-		await api.post("/auth/signin", { username, password }),
-};
-
-export default AuthAPI;
--- src/services/trainee.service.js
+++
@@ -1,7 +0,0 @@
-import api from "./api";
-
-const TraineeAPI = {
-	getTrainees: async () => await api.get("/trainees"),
-};
-
-export default TraineeAPI;
